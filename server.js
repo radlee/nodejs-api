87a2ce089b21f2840797d01cb93ce3fa86ec8378
radlee <prof.lolito@gmail.com>
2020-04-11 12:31:29 +0200

    Installed and configured express

--- server.js
+++ server.js
@@ -1,41 +1,17 @@
 const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  
-  if (url === '/message' && method === 'POST' ) {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk)
-    });
-    return req.on('end', ()=> {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302; 
-        res.setHeader('Location', '/');
-        return res.end()
-      });
-    });
-    ;
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Complext Page</title><head>');
-  res.write('<body><h1>Hello From NodeJs Server</h1></body>');
-  res.write('</html>');
-  res.end();
- 
+app.use((req, res, next) => {
+    console.log('In the Middleware');
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log('In the other Middleware');
+    res.send('<h1>Hello From Express</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(5000);
