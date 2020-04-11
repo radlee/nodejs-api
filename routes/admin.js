78d5e164baa2b67d081b804b62151e072ee5f252
radlee <prof.lolito@gmail.com>
2020-04-11 18:13:46 +0200

    Added Style and configured routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,16 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router()
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // console.log('In the other Middleware');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -11,10 +12,8 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(5000)
