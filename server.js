b59217b991b0e9579fecbc1a9d7d1f4e3ab0f291
radlee <prof.lolito@gmail.com>
2020-04-11 15:10:43 +0200

    Parsing Incoming Requests

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/', (req, res, next) => {
-    console.log('This always Runs');
+    // console.log('This always Runs');
     next();
 })
 
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In the other Middleware');
-    res.send('<h1>The Add Product Page</h1>')
+    // console.log('In the other Middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/', (req, res, next) => {
-    console.log('In the other Middleware');
+    // console.log('In the other Middleware');
     res.send('<h1>Hello From Express</h1>')
 });
 
