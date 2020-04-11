a619dcbccb3e0f6f66450f4107f8af7d8af9769b
radlee <prof.lolito@gmail.com>
2020-04-11 15:41:23 +0200

    Router

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use('/', (req, res, next) => {
-    // console.log('This always Runs');
-    next();
-})
-
-
-app.use('/add-product', (req, res, next) => {
-    // console.log('In the other Middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    // console.log('In the other Middleware');
-    res.send('<h1>Hello From Express</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(5000)
